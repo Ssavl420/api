acd3e09dcc497e2f5cc1fe8e666e6fb82ec3a5a4
Sergey Savlevich <sa.savlevich@gmail.com>
2023-06-15 01:10:12 +0700

    change url in js

--- js/index.js
+++ js/index.js
@@ -7,8 +7,8 @@ const message = document.querySelector('.message');
 btnActivity.addEventListener('click', getActivity)
 
 function getActivity() {
-   console.log('Функция запущена')
-   fetch('http://www.boredapi.com/api/activity/')
+
+   fetch('https://www.boredapi.com/api/activity/')
       .then(data => data.json())
       .then(response => {
          // if(response.status !== 'success') { //success не робит!. undefined
