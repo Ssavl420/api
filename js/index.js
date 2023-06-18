ebf261af6c76618c005bb5686eaeef34c1222ecb
Sergey Savlevich <sa.savlevich@gmail.com>
2023-06-18 16:24:48 +0700

    add some property

--- js/index.js
+++ js/index.js
@@ -6,18 +6,73 @@ const message = document.querySelector('.message');
 
 btnActivity.addEventListener('click', getActivity)
 
+// function getActivity() {
+//    // fetch('https://www.boredapi.com/api/activity/')
+
+//    (async () => {
+//       // Шаг 1: начинаем загрузку fetch, получаем поток для чтения
+//       let response = await fetch('https://www.boredapi.com/api/activity/');
+      
+//       const reader = response.body.getReader();
+      
+//       // Шаг 2: получаем длину содержимого ответа
+//       const contentLength = +response.headers.get('Content-Length');
+      
+//       // Шаг 3: считываем данные:
+//       let receivedLength = 0; // количество байт, полученных на данный момент
+//       let chunks = []; // массив полученных двоичных фрагментов (составляющих тело ответа)
+//       while(true) {
+//          const {done, value} = await reader.read();
+//          console.log("Загрузка")
+//          if (done) {
+//          break;
+//          }
+      
+//          chunks.push(value);
+//          receivedLength += value.length;
+      
+//          console.log(`Получено ${receivedLength} из ${contentLength}`)
+//       }
+      
+//       // Шаг 4: соединим фрагменты в общий типизированный массив Uint8Array
+//       let chunksAll = new Uint8Array(receivedLength); // (4.1)
+//       let position = 0;
+//       for(let chunk of chunks) {
+//          chunksAll.set(chunk, position); // (4.2)
+//          position += chunk.length;
+//       }
+      
+//       // Шаг 5: декодируем Uint8Array обратно в строку
+//       let result = new TextDecoder("utf-8").decode(chunksAll);
+      
+//       // Готово!
+//       let commits = JSON.parse(result);
+//       alert(commits[0].author.login);
+//       })()
+
+// }
 function getActivity() {
 
-   fetch('https://www.boredapi.com/api/activity/')
-      .then(data => data.json())
-      .then(response => {
-         // if(response.status !== 'success') { //success не робит!. undefined
-         //    console.log('response.status')
-         //    return null;
-         // }
-         tagBodyHtml.classList.add('success');
-         title.innerText = `Ура, теперь не скучно 🔥`;
-         message.innerText = response.activity;
-      })
-}
+   btnActivity.setAttribute("disabled", "disabled");
+   message.style.cssText = 'opacity: 0.2;';
 
+   fetch('https://www.boredapi.com/api/activity/')
+   .then(data => {
+      
+      if (data.status !== 200) {
+         return null;
+      }
+
+      return data.json();
+   })
+   .then(response => {
+
+      tagBodyHtml.classList.add('success');
+      title.innerText = `Ура, теперь не скучно 🔥`;
+      message.innerText = response.activity;
+
+      btnActivity.removeAttribute("disabled", "disabled");
+      message.style.cssText = 'opacity: 1;';
+
+   })
+}
